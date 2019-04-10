6aa6396ac3dc0956256ea3c098e655f325d87608
Vinay Mehta <khushbu.goyal@berylsystems.com>
2019-04-10 17:44:37 +0530

    tooltip UI fixes

--- app/assets/javascripts/advanced.js
+++ app/assets/javascripts/advanced.js
@@ -34,4 +34,18 @@ $(".check_tags input[type='checkbox']").change(function(){
   }
 });
 
-$('[data-toggle="tooltip"]').tooltip();
+$('[data-toggle="tooltip"]').tooltip({ trigger: "manual", html: true, animation:false})
+    .on("mouseenter", function () {
+        var _this = this;
+        $(this).tooltip("show");
+        $(".tooltip").on("mouseleave", function () {
+            $(_this).tooltip('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".tooltip:hover").length) {
+                $(_this).tooltip("hide");
+            }
+        }, 300);
+});
